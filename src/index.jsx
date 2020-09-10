15357adc3f06a6747e60870d39d998704f7f9fcc
robertpan226 <robertpan226@gmail.com>
2020-09-09 20:57:54 -0400

    Changed github pages URL

--- src/index.jsx
+++ src/index.jsx
@@ -15,7 +15,7 @@ class App extends React.Component<{}, {}> {
         <div className={styles.test}>
           <h1 >Hello!</h1>
         </div>
-        <img src="https://d1vopzf7po8fvg.cloudfront.net/nyancat.gif" alt="loading..." />
+        <img src="https://media1.tenor.com/images/89626c3be83744605c413c47161ffa72/tenor.gif?itemid=4569620" alt="loading..." />
       </div>
     )
   }
